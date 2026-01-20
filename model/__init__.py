1f0fe51ae3a7f412492395da816ed19a317f43fc
Josh Herrera <bluedude227@gmail.com>
2026-01-20 22:27:48 +0000

    Remove runtime artifacts that aren't ready

--- model/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2026 Quant Civil
-
-"""Runtime artifacts for the next-gen model backends."""
-
-from .runtime_numpy import NumpySegmenter, RuntimeMeta, load_runtime_model
-from .runtime_backend import load_runtime
-
-__all__ = ["NumpySegmenter", "RuntimeMeta", "load_runtime_model", "load_runtime"]
